d4aa4c65d5712f470f7a532bf69f14a272943382
Gianmarco Midena <gianmarco.midena@gmail.com>
2022-06-11 11:03:27 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='drug-combo-data-prep',
+    version='0.1.0',
+    packages=['test', 'test.partitioning', 'dc_dataprep', 'dc_dataprep.partitioning'],
+    url='https://github.com/GianmarcoMidena/drug-combo-data-prep',
+    license='MIT',
+    author='Gianmarco Midena',
+    author_email='gianmarco.midena@gmail.com',
+    description='Data preparation for drug combination prediction'
+)
